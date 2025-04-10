1fd02277ddc1fbabd9b911c9f9733a9c9efe7af7
Thomas <jpt215@exeter.ac.uk>
2025-04-10 16:30:15 +0100

    retrieve features from models

--- scripts/mlr3_modular.R
+++ scripts/mlr3_modular.R
@@ -58,7 +58,7 @@ library(sf)
 library(patchwork)  # For arranging plots
 library(future)
 
-future::plan("sequential", workers = 40)
+future::plan("multisession", workers = 40)
 
 #' Create hyperparameter tuning space for xgboost
 #' @param prefix character prefix to add to parameter names - useful for
@@ -104,31 +104,31 @@ set.seed(42)  # For reproducibility
 
 #dataset
 samples_metrics<- read_sf("Data/soil_samples_w_complete_metrics.fgb")
-train_data_rm<- select(train_data, where(~!any(is.na(.))))
+#train_data_rm<- select(train_data, where(~!any(is.na(.))))
 train_data_clean<- select(converted_data, -"wmean_acd_lidar_3", -"wmean_GF_3")%>%
   drop_na()
 
 # Define Simulation Configurations
 simulations <- list(
-  #list(feature_suffixes = c("_1"), sim_id = 1),
-  #list(feature_suffixes = c("_1", "_2"), sim_id = 2),
-  list(feature_suffixes = c("_3"), sim_id = 3)
-  #list(feature_suffixes = c("_5"), sim_id = 4),
-  #list(feature_suffixes = c("_4", "_5"), sim_id = 5),
-  #list(feature_suffixes = c("_1", "_5"), sim_id = 6),
-  #list(feature_suffixes = c("_1", "_2", "_4", "_5"), sim_id = 7),
-  #list(feature_suffixes = c("_3", "_4", "_5"), sim_id = 8),
-  #list(feature_suffixes = c("_1", "_2", "_4", "_5", "_3"), sim_id = 9),
-  #list(feature_suffixes = c("_1", "_2", "_4", "_3"), sim_id = 10),
-  #list(feature_suffixes = c("_1", "_2", "_3"), sim_id = 11)
+  list(feature_suffixes = c("_1"), sim_id = 1),
+  list(feature_suffixes = c("_1", "_2"), sim_id = 2),
+  list(feature_suffixes = c("_3"), sim_id = 3),
+  list(feature_suffixes = c("_5"), sim_id = 4),
+  list(feature_suffixes = c("_4", "_5"), sim_id = 5),
+  list(feature_suffixes = c("_1", "_5"), sim_id = 6),
+  list(feature_suffixes = c("_1", "_2", "_4", "_5"), sim_id = 7),
+  list(feature_suffixes = c("_3", "_4", "_5"), sim_id = 8),
+  list(feature_suffixes = c("_1", "_2", "_4", "_5", "_3"), sim_id = 9),
+  list(feature_suffixes = c("_1", "_2", "_4", "_3"), sim_id = 10),
+  list(feature_suffixes = c("_1", "_2", "_3"), sim_id = 11)
 )
 
 # Define learners and search space Configurations
 students <- list(
-  list(learner = lrn("regr.ranger"), SS = "regr.ranger.default"),
-  #list(learner = lrn("regr.glm", id = "glm1"), SS=NULL),
+  list(learner = lrn("regr.ranger", importance = "impurity"), SS = "regr.ranger.default"),
+  list(learner = lrn("regr.glm"), SS=NULL)
   #list(learner = lrn("regr.kknn"), SS = "regr.kknn.default"),
-  list(learner = lrn("regr.rpart"), SS = "regr.rpart.default")
+  #list(learner = lrn("regr.rpart"), SS = "regr.rpart.default")
   #list(learner = lrn("regr.svm"), SS = "regr.svm.default")
   #list(learner = lrn("regr.xgboost"), SS = xgboost_ps)
 )
@@ -192,7 +192,7 @@ learners = learns_list
 resamplings = rsmp("spcv_coords", folds = 3)
 
 design = benchmark_grid(task_list, learners, resamplings)
-bmr = progressr::with_progress(benchmark(design, store_models = TRUE))#, encapsulate = "evaluate"))
+bmr = progressr::with_progress(benchmark(design, store_models = TRUE))
 bmr$aggregate()
 
 ### extract results ####
@@ -205,6 +205,44 @@ x <- bmr$aggregate() %>%
   ungroup()
 
 ### resample best learners ####
-bmr$score()$learner[[3]]$importance()
+bmr$score()$learner[[2]]$importance()
 
+features_function<- function (i){
+  features<-extract_inner_fselect_results(bmr)$features[[i]]
+  return(features)
+}
+
+
+y<- x$nr
+# y
+#[1]  2  5  9 11 14 16 19 22 27 30 31
+z<- c(y*3-2,y*3-1, y*3)
+#> z
+#[1]  4 13 25 31 40 46 55 64 79 88 91  5 14 26 32 41 47 56 65 80 89 92  6 15 27 33 42 48 57 66 81 90 93
+retained_features<- lapply(z, features_function)
+collapsed_unique <- unique(unlist(retained_features))
+
+# Step 1: Flatten the list
+all_values <- unlist(retained_features)
+
+# Step 2: Count frequencies
+value_counts <- table(all_values)
+
+# Step 3: Convert to data frame
+df <- as.data.frame(value_counts)
+colnames(df) <- c("value", "count")
+
+# Step 4: Apply division rules
+df$adjusted_count <- with(df, ifelse(
+  grepl("_1$", value), count / 7,
+  ifelse(grepl("_2$", value), count / 5,
+         ifelse(grepl("_3$", value), count / 5,
+                ifelse(grepl("_4$", value), count / 5,
+                       ifelse(grepl("_5$", value), count / 6, count)
+                )))))
+
+df <- df[order(-df$adjusted_count), ]
+
+# Print the sorted table
+print(df)
 ### plots and analysis ####
