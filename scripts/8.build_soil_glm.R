d1643babc5c752cf39a11a6192c0c31445c4dd60
Thomas <jpt215@exeter.ac.uk>
2026-01-12 17:19:36 +0000

    clean PCA

--- scripts/8.build_soil_glm.R
+++ scripts/8.build_soil_glm.R
@@ -6,9 +6,10 @@ install.packages("ggrepel")
 install.packages("broom")
 install.packages("ggeffects")
 install.packages("sjPlot")
-install.packages("vegan")
 install.packages("modelsummary")
 install.packages("pandoc")
+#install.packages("factoextra")
+library(factoextra)
 library(performance)
 library(sf)
 library(dplyr)
@@ -21,7 +22,6 @@ library(ggrepel)
 library(stringr)
 library(purrr)
 library(broom)
-library(vegan)
 library(ggeffects)
 library(modelsummary)
 
@@ -47,10 +47,10 @@ theme_beautiful <- function() {
         hjust = 0.5,
         color = "black"
       ),
-      legend.text = element_text(size = 8, color = "black"),
-      legend.title = element_text(size = 8, color = "black"),
+      legend.text = element_text(size = 18, color = "black"),
+      legend.title = element_text(size = 18, color = "black"),
       legend.position = c(0.9, 0.9),
-      legend.key.size = unit(0.9, "line"),
+      legend.key.size = unit(3, "line"),
       legend.background = element_rect(
         color = "black",
         fill = "transparent",
@@ -82,8 +82,6 @@ fp<-"C:/Users/jpt215/OneDrive - University of Exeter/PhD_Data/Soil_manuscript_da
 #### load data
 samples_metrics<- read_rds(file.path(fp, "soil_samples_w_complete_metrics.rds"))
 
-train_data<- samples_metrics|>
-  dplyr::rename(percC = percC_5)
 
 
 
@@ -106,16 +104,15 @@ converted_data <- convert_to_numeric_or_factor(samples_metrics)
 #### format table for glm ##### 
 df <- converted_data%>%
   st_drop_geometry()%>%
-  select(-"percN_5", -"x15N_5", -"fhd_normal_2", -"pground_3", -"...15")|>
-  select( -c('VCI_3', 'ent_3', 'zentropy_3'))# , 'GF_3', 'VCI_3'
+  select(-"percN_5", -"x15N_5",  -"...15", -"area_3", - "acd_lidar_3")
+
 
 
 # weighted means of soil grids
 compute_rowwise_weighted_means <- function(df) {
-  filter_values<- c("0.5cm", "5.15cm", "15.30cm")
   
-  df_long <- df %>%
-    mutate(row_id = row_number()) %>%
+  df_long <- df |>
+    mutate(row_id = row_number()) |>
     pivot_longer(
       cols = matches("*_mean_1$"),
       names_to = "variable",
@@ -128,7 +125,7 @@ compute_rowwise_weighted_means <- function(df) {
       depth_bottom = as.numeric(str_extract(depth_range, "(?<=\\.)\\d+(?=cm)")),
       depth_thickness = depth_bottom - depth_top
     )|>
-    dplyr::filter(depth_range %in% filter_values)
+    dplyr::filter(depth_bottom<31)
   
   df_weighted <- df_long %>%
     group_by(row_id, value_type) %>%
@@ -160,7 +157,7 @@ df_numeric <- df_means |>
   mutate(across(where(~!is.numeric(.)), ~ as.numeric(as.factor(.x))))|>  # Convert non-numeric to numeric factors
   select(where(is.numeric))|>                                           # Select all numeric columns
   select(where(~ sd(.x, na.rm = TRUE) != 0))|>
-  select(-matches("^zq|cover_z|pai_z|pavd_z|zpcum"))|>
+  select(-matches("^zq|cover_z|pai_z|pavd_z|zpcum|iskew|ikurt|ipcum|itot"))|>
   drop_na()
 
 
@@ -170,85 +167,85 @@ df_CS <- df_numeric %>%
   select(matches("(_2$|_3$|_4$)"))
 
 #Run PCA on filtered data
-rda.out <- vegan::rda(df_numeric, scale = TRUE)
-# add scores()
-rda_scores <- vegan::scores(rda.out)
-# add biplot()
-biplot(rda.out, display = c("species"), type = "text")
+pca <- prcomp(df_numeric, center = TRUE, scale. = TRUE)
 
-#Run PCA on CS data
-CSrda.out <- vegan::rda(df_CS, scale = TRUE)
-# add scores()
-CSrda_scores <- vegan::scores(CSrda.out)
-# add biplot()
-biplot(CSrda.out, display = c("species"), type = "text")
+pca_CS<-prcomp(df_CS, center = TRUE, scale. = TRUE)
 
-##bi_plot with cos2 scores
+# Plot biplot with contribution coloring
+full_plot<-fviz_pca_var(
+  pca,
+  repel = TRUE,
+  geom = c("arrow", "text"),
+  col.var =  "contrib",
+  label = "var"
+)+
+  scale_color_viridis_c(option = "C")+
+  labs(title = "A)", color = "Contribution")+
+  theme(legend.text = element_text(size = 18, color = "black"),
+        legend.title = element_text(size = 18, color = "black"),
+        legend.position = "right",
+        legend.key.size = unit(3, "line"),
+        axis.text = element_text(size = 18, color = "black"),
+        axis.title = element_text(size = 18, color = "black"),
+        plot.title = element_text(
+          size = 24,
+          color = "black"
+        ))
+full_plot$layers[[3]]$aes_params$alpha <- 0
+full_plot$layers[[2]]$aes_params$linewidth <- 1
+full_plot$layers[[1]]$aes_params <- list( nudge_x = 0.02, nudge_y = 0.02, size = 8,max.overlaps = 5, 
+                                          box.padding = 0.35, point.padding = 0.5, colour = "black")
+full_plot$layers<- c(full_plot$layers[[2]], full_plot$layers[[4]], full_plot$layers[[5]], full_plot$layers[[1]])
 
-bi_plot_func<- function(PCA_scores) {
-  
-  # Extract species (variable) scores for the first two axes
-  species_scores <- as.data.frame(PCA_scores$species[, 1:2])
-  colnames(species_scores) <- c("PC1", "PC2")
-  species_scores$var <- rownames(species_scores)
-  
-  # Compute cos2 = (loading^2) / (sum of squares of all loadings for that variable)
-  species_scores <- species_scores %>%
-    mutate(
-      cos2 = PC1^2 + PC2^2
-    )
-  
-  # Optional: scale arrows by cos2 for better visual emphasis
-  species_scores <- species_scores %>%
-    mutate(
-      PC1_scaled = PC1 * cos2,
-      PC2_scaled = PC2 * cos2
-    )
-  species_scores <- species_scores %>%
-    mutate(loading_magnitude = sqrt(PC1_scaled^2 + PC2_scaled^2))
-  
-  top_vars <- species_scores %>%
-    slice_max(loading_magnitude, n = 20)
-  
-  
-  # Plot biplot with cos2 coloring
-  p<-ggplot(species_scores, aes(x = PC1_scaled, y = PC2_scaled, label = var)) +
-    geom_segment(aes(x = 0, y = 0, xend = PC1_scaled, yend = PC2_scaled, color = cos2),
-                 arrow = arrow(length = unit(0.2, "cm")), linewidth = 1) +
-    geom_text_repel(nudge_x = 0.02, nudge_y = 0.02, size = 8,max.overlaps = 5, 
-                    box.padding = 0.35, point.padding = 0.5) +
-    scale_color_viridis_c(option = "C") +
-    labs(title = "B",
-         x = "PC1",
-         y = "PC2",
-         color = "cos²") +
-    theme_minimal()
-  return (p)
-}
+ggsave("Plots/PCA_full_plot.png", plot = full_plot, width = 20, height = 20, dpi = 300)
 
-full_plot<- bi_plot_func(rda_scores)
-ggsave("Plots/PCA_full_plot.png", plot = full_plot, width = 20, height = 18, dpi = 300)
-CS_plot<- bi_plot_func(CSrda_scores)
+CS_plot<-fviz_pca_var(
+  pca_CS,
+  repel = TRUE,
+  geom = c("arrow", "text"),
+  col.var =  "contrib",
+  label = "var"
+)+
+  scale_color_viridis_c(option = "C")+
+  labs(title = "B)", color = "Contribution")+
+  theme(legend.text = element_text(size = 18, color = "black"),
+        legend.title = element_text(size = 18, color = "black"),
+        legend.position = "right",
+        legend.key.size = unit(3, "line"),
+        axis.text = element_text(size = 18, color = "black"),
+        axis.title = element_text(size = 18, color = "black"),
+        plot.title = element_text(
+          size = 24,
+          color = "black"
+        ))
+CS_plot$layers[[3]]$aes_params$alpha <- 0
+CS_plot$layers[[2]]$aes_params$linewidth <- 1
+CS_plot$layers[[1]]$aes_params <- list( nudge_x = 0.02, nudge_y = 0.02, size = 8,max.overlaps = 5, 
+                                        box.padding = 0.35, point.padding = 0.5, colour = "black")
+CS_plot$layers<- c(CS_plot$layers[[2]], CS_plot$layers[[4]], CS_plot$layers[[5]], CS_plot$layers[[1]])
+
 ggsave("Plots/PCA_CS_plot.png", plot = CS_plot, width = 20, height = 18, dpi = 300)
+
+
+
 
 ### PCA to reduce predictors
 
+loadings <- as.data.frame(pca$rotation)
+loadings_abs<- abs(loadings)|>
+  tibble::rownames_to_column("var")
 
-# Get loadings (rotation matrix: variables x components)
-loadings <- as.data.frame(vegan::scores(rda.out, choices = 1:20, display = "species")) # Take absolute values for proximity
 
-# Create an empty vector to collect selected variable names
+
 selected_vars <- c()
 
 # Loop through PCs 1 to 20
-for (i in 1:ncol(loadings)) {
+for (i in 2:ncol(loadings_abs)) {
   
   # Get sorted variable names by loading (descending) for this PC
-  pc<- names(loadings)[1]
+  pc<- names(loadings_abs[i])
   
-  ranked_vars <- loadings%>%
-    as.data.frame() %>%
-    tibble::rownames_to_column("var") %>%
+  ranked_vars <- loadings_abs%>%
     arrange(desc(.data[[pc]])) %>%
     pull(var)
   # Find the first variable not already selected
@@ -260,9 +257,9 @@ for (i in 1:ncol(loadings)) {
       break
     }
     
-    # Stop once we have 20 unique variables
-    if (length(selected_vars) >= 20) break
-  }}
+  }# Stop once we have 20 unique variables
+  if (length(selected_vars) >= 20) break
+}
 #df_selected <- df_numeric[, selected_vars]
 df_selected <- as.data.frame(st_drop_geometry(df_means))[, selected_vars]
 
@@ -290,35 +287,16 @@ lines(resid(glm.gam.id, type='response'), col='red')
 glm_model <- glm(glm_formula, data = df_means, family = Gamma(link="log"))
 
 summary(glm_model)
-long_names<-c("LAD_3" = "Leaf Area Density (Airborne Lidar)",
-              "wmean_soc_1"  = "Soil Organic Carbon (Online)",
-              "year_of_last_fire_1" = "Year of Most Recent Fire (Online)",
-              "imean_3" = "Mean Intensity of Lidar Returns (Airborne Lidar)",
-              "p1th_3"  = "Percentage 1st Returns (Airborne Lidar)",
-              "wmean_sand_1"  =  "Sand content (Online)",
-              "isd_3" = "Standard Deviation of Lidar Return Intensity (Airborne Lidar)",
-              "secondary_vegetation_age_2023_1" = "Age of Secondary Vegetation (Online)",
-              "classification_2023_1"   = "Classification of Forest Degredation (Online)",
-              "itot_3"  = "Total Lidar Return Intensity (Airborne Lidar)",
-              "pgap_theta_2" = "Pgap(theta) of Canopy (Spaceborne Lidar)",
-              "pzabovezmean_3"  = "Percentage of Returns Above Mean Canopy Height (Airborne Lidar)",
-              "imax_3" = "Maximum Intensity of Lidar Returns (Airborne Lidar)",
-              "zkurt_3" = "Kurtosis of Canopy Height (Airborne Lidar)",
-              "ipground_3" = "Percentage of Intensity Returned by Ground (Airborne Lidar)",
-              "clumping.index_4" = "Clumping Index (Hemispherical Photography)",
-              "wmean_cec_1" = "Cation Exchange Capacity (Online)",
-              "Seasonal_NDVI_Change_1"  = "Seasonall Difference in NDVI (Online)", 
-              "canopy.openness_4" = "Canopy Openness (Hemispherical Photography)",
-              "wmean_nitrogen_1" = "Total Nitrogen (Online)")
+
 table_s3<-modelsummary(list("20 variable GLM Summary" = glm_model), 
-             fmt = set_sf, 
-             shape = term ~ model + statistic,
-             coef_omit = "Intercept",
-             estimate  = "{estimate}",
-             statistic = c("{std.error}", "{p.value}"),
-             gof_omit = "Num.Obs|BIC|Log.Lik.|F",
-             exponentiate = TRUE,
-             output = "Plots/Table_S3.docx"
+                       fmt = set_sf, 
+                       shape = term ~ model + statistic,
+                       coef_omit = "Intercept",
+                       estimate  = "{estimate}",
+                       statistic = c("{std.error}", "{p.value}"),
+                       gof_omit = "Num.Obs|BIC|Log.Lik.|F",
+                       exponentiate = TRUE,
+                       output = "Plots/Table_S3.docx"
 )
 TableS3<- tidy(glm_model, exponentiate = TRUE)
 write.csv(TableS3, "Plots/TableS3.csv")
@@ -374,8 +352,8 @@ ggsave("Plots/effect_sizes.png", plot = effect_plot, width = 15, height = 10, dp
 #####  further investigation (should be done manually)
 
 ##remove predictors with high colinearity and modelled data 
-simple_glm <-glm(formula = percC_5 ~ LAD_3 +  imean_3 + p1th_3 + 
-                    itot_3 + pzabovezmean_3 + zkurt_3, 
+simple_glm <-glm(formula = percC_5 ~ zsd_3 + zskew_3 + zkurt_3 + 
+                   isd_3  + p1th_3,
                  family = Gamma(link = "log"), data = df_means)
 check_model(simple_glm)
 
@@ -414,32 +392,30 @@ table_s5<-modelsummary(list("Reduced GLM Summary" = simple_glm),
                        statistic = c("{std.error}", "{p.value}"),
                        gof_omit = "Num.Obs|BIC|Log.Lik.|F",
                        exponentiate = TRUE,
-                       coef_rename = long_names,
                        output = "Plots/Table_Ss.docx"
 )
 
 TableS5<-tidy(simple_glm, exponentiate = TRUE)
 write.csv(TableS5, "Plots/TableS5.csv")
 
-p5<-plot(predict_response(simple_glm, terms= "LAD_3")) +
-labs( title = "a)" , x= "Mean Leaf Area Density", y= "Mean %C")+
+p5<-plot(predict_response(best_model, terms= "zsd_3")) +
+  labs( title = "a)" , x= "Standard Deviation of Canopy Height", y= "Mean %C")+
   theme_minimal(base_size = 24)
-p6<-plot(predict_response(simple_glm, terms= "pzabovezmean_3")) +
-  labs( title = "b)" , x= "Percentage of Lidar Returns Above Mean Canopy Height", y= "Mean %C")+
+p6<-plot(predict_response(best_model, terms= "zskew_3")) +
+  labs( title = "b)" , x= "Skew of Canopy Height", y= "Mean %C")+
   theme_minimal(base_size = 24)
-p7<-plot(predict_response(simple_glm, terms= "imean_3")) +
-  labs( title = "c)" , x= "Mean Intensity of Lidar Returns", y= "Mean %C")+
+p7<-plot(predict_response(best_model, terms= "isd_3")) +
+  labs( title = "c)" , x= "Standard Deviation of Lidar Return Intensity", y= "Mean %C")+
   theme_minimal(base_size = 24)
-p8<-plot(predict_response(simple_glm, terms= "zkurt_3")) +
+p8<-plot(predict_response(best_model, terms= "zkurt_3")) +
   labs( title = "d)" , x= "Kurtosis of Canopy Height Distribution", y= "Mean %C")+
   theme_minimal(base_size = 24)
-p9<-plot(predict_response(simple_glm, terms= "p1th_3")) +
+p9<-plot(predict_response(best_model, terms= "p1th_3")) +
   labs( title = "e)" , x= "Percentage 1st Returns", y= "Mean %C")+
   theme_minimal(base_size = 24)
-p10<-plot(predict_response(simple_glm, terms= "itot_3 ")) +
-  labs( title = "f)" , x= "Total Intensity of Lidar Returns", y= "Mean %C")+
-  theme_minimal(base_size = 24)
-p<- (p5+p6 )/(p7 +p8)/( p9+ p10)
 
+p<- (p5+p6 )/(p7 +p8)/( p9)
+
+p
 ggsave('plots/marginal_effects.png', p, width = 20, height = 18, dpi = 300)
 
